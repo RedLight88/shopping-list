addae19c2c2b703a762db6f21bb27d84f9fa9225
Robert Gabriel <robert.gm644@gmail.com>
2024-07-09 16:45:26 +0300

    added clear all

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ if (currentHour < 12) {
   greeting = 'Good evening';
 }
 greet.textContent = greeting;
-//
+//////////////////////////////////////////////////////
 
 const itemInput = document.querySelector('.input input');
 const addBtn = document.querySelector('.input button');
 const cnt = document.querySelector('.container ul');
 
+const clear = document.querySelector('.clearAll');
+const itemFilter = document.querySelector('.filter');
 function addItem() {
   if (itemInput.value.trim() === '') {
     alert('Please fill in the input with an item');
@@ -29,7 +31,10 @@ function addItem() {
   cnt.appendChild(li);
   itemInput.value = '';
 
+  checkUI();
+
   button.addEventListener('click', remove);
+  clear.addEventListener('click', clearAll);
 }
 
 function createButton() {
@@ -38,11 +43,50 @@ function createButton() {
   button.textContent = 'x';
   return button;
 }
-
 function remove(e) {
   if (e.target.classList.contains('removeBtn')) {
-    e.target.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.remove();
+    }
   }
 }
 
+function clearAll() {
+  if (cnt) {
+    while (cnt.firstChild) {
+      cnt.removeChild(cnt.firstChild);
+    }
+  } else {
+    console.error('The container element is null or not found.');
+  }
+}
+
+function checkUI() {
+  const items = document.querySelectorAll('li');
+
+  if (items.length === 0) {
+    clear.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clear.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
+function filterItems(e) {
+  const items = document.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
 addBtn.addEventListener('click', addItem);
+itemFilter.addEventListener('input', filterItems);
